e341057e4d6a2dc092a0a343932f2e5f1cd486ca
Márcio Moreira <marcio.moreira@abacusweb.com.br>
2024-02-16 09:34:35 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$host = "postgres";
+$db = "postgres";
+$user = $db;
+$password = $db;
+
+try {
+	$dsn = "pgsql:host=$host;port=5432;dbname=$db;";
+	// make a database connection
+	$pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+	if ($pdo) {
+		echo "Connected to the $db database successfully!";
+	}
+} catch (PDOException $e) {
+	die($e->getMessage());
+} finally {
+	if ($pdo) {
+		$pdo = null;
+	}
+}
